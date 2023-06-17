c24b146be89321828fc18e4399987161b162c4c6
Cfp <chaosflo44plus@gmail.com>
2023-06-17 19:11:23 +0200

    server.properties and paper sha256 check

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-import { mkdir, stat, writeFile } from "fs/promises";
+import { mkdir, stat, writeFile, readFile } from "fs/promises";
 import { join } from "path";
+import { createHash } from "crypto";
 
-const config = JSON.parse(await readFile("config.json"));
+const config = JSON.parse(await readFile("bootstrap/config.json"));
 
 async function download(url, file) {
 	console.log(`Downloading ${url} into ${file}`);
 	await writeFile(join(config.server_dir, file), Buffer.from(await fetch(url).then(res => res.arrayBuffer())));
+}
+
+async function sha256(path) {
+	const buff = await readFile(path);
+	const hash = createHash("sha256").update(buff).digest("hex");
+	return hash;
 }
 
 class PaperAPI {
@@ -52,10 +59,16 @@ async function updatePaper() {
 	const builds = await PaperAPI.getBuildsForVersion("paper", config.mc_version);
 	const latestBuild = builds.builds[builds.builds.length - 1];
 	const buildInfo = await PaperAPI.getBuildInfo("paper", config.mc_version, latestBuild);
+	if(await exists(join(config.server_dir, "paper.jar")) && await sha256(join(config.server_dir, "paper.jar")) == buildInfo.downloads.application.sha256) return;
 	await PaperAPI.downloadBuild("paper", config.mc_version, latestBuild, buildInfo.downloads.application.name, "paper.jar");
+}
+
+async function copyServerProperties() {
+	if(await exists("bootstrap/server.properties")) await writeFile(join(config.server_dir, "server.properties"), await readFile("bootstrap/server.properties"));
 }
 
 await createBasicServerStructure();
 await acceptEULA();
 await updatePaper();
+await copyServerProperties();
 for(const resource of config.spiget_resources) await downloadSpigetResource(resource, join("plugins", resource + ".jar"));
